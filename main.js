6715ad83ef0cebfca6530fe8512c422418aa0f78
cyrine <cyrine.rihane@gmail.com>
2017-11-17 11:11:15 +0100

    final change

--- main.js
+++ main.js
@@ -1,83 +1,95 @@
-$(document).ready(function (){
-	var positionElt = $(".position");
-	function getCelcius(temp){
-		var cel = (temp - 32) * 5/9;
-		return cel;
-	};
+const WEATHER_API = 'https://fcc-weather-api.glitch.me/api/current'
+let positionElt = $(".position");
+let tempInCelcius;
+let tempInFahrenheit;
 
-	function getFahrenheit(temp){
-		var feh = (temp * 9/5 + 32)
-		return feh;
-	};
+function getWeatherData(lat, lon) {
 
-	function getLocation() {
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(showPosition);
-		} else {
-			positionElt.html("Geolocation is not supported by this browser.")
+	$.ajax({
+		url: WEATHER_API,
+		data: {
+			lat: lat,
+			lon: lon
+		},
+		dataType: 'json',
+		success: function (data) {
+			tempInFahrenheit = (data.main.temp * 9/5 + 32)
+			tempInCelcius = data.main.temp
+			updateUI(data, {lat: lat, lon: lon});
+		},
+		error: function (xhr, error) {
+			console.error(error);
 		}
+	});
+	
+}
+
+function getLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(
+			function (position) { getWeatherData(position.coords.latitude, position.coords.longitude) },
+			function (error) { console.error(error) },
+		);
+	} else {
+		positionElt.html("Geolocation is not supported by this browser.")
 	}
+}
 
-	function showPosition(position) {
+function convertTemperature (){
+	if ($('.temp').hasClass('celsius')) {
+		$('.temp .deg').text(tempInFahrenheit)
+		$('.temp .unit').text('F')
+		$('.temp').attr('class', 'temp fahrenheit');
 
-		$.get('https://fcc-weather-api.glitch.me/api/current?lat='+
-			position.coords.latitude +'&lon=' + position.coords.longitude,
-			function(data) {
-				console.log(data)
-				$('.loader').css('display','none')
-				$('.location').text(data.name + ', ' + data.sys.country);
-				$('.temp .deg').text(data.main.temp)
-				$('.temp .unit').text('C')
-				$('.temp').css('display','inline')
-				$('.windSpeed').text(data.wind.speed + ' mph')
-				$('.humidity').text(data.main.humidity + '%')
-				$('.tempMinMax').text(data.main.temp_min + '°' + '/' + data.main.temp_max + '°')
-				$('.pressure').text(data.main.pressure + ' SI')
-				$('.visibility').text(data.visibility + ' mi')
-				$('.image').attr('src' , "https://maps.googleapis.com/maps/api/staticmap?center=" + position.coords.latitude + "," + position.coords.longitude + "&zoom=13&size=300x300&sensor=false") 
-				$('.temp a').click(function(){
-
-					if ($('.temp').hasClass('celsius')) {
-						$('.temp .deg').text(getFahrenheit(data.main.temp))
-						$('.temp .unit').text('F')
-						$('.temp').attr('class', 'temp fahrenheit');
-
-					} else {
-						$('.temp .deg').text(data.main.temp)
-						$('.temp .unit').text('C')
-						$('.temp').attr('class', 'temp celsius');
-					}
-
-				});
-				$('.weather').text(data.weather[0].description);
-
-				switch (data.weather[0].description) {
-					case "few clouds" :
-					$('.iconDesc').append('<div class="icon cloudy"><div class="cloud"></div><div class="cloud"></div></div>');
-					break;
-					case "thunderstorm" :
-					$('.iconDesc').append('<div class="icon thunder-storm"><div class="cloud"></div><div class="lightning"></div></div>');
-					break;
-					case "scattered clouds" :
-					$('.iconDesc').append('<div class="icon cloudy"><div class="cloud"></div><div class="sun"></div><div class="rain"></div></div>');
-					break;
-					case "sunny" :
-					$('.iconDesc').append('<div class="icon sunny"><div class="sun"></div></div>');
-					break;
-					case "rainy" :
-					$('.iconDesc').append('<div class="icon rainy"><div class="cloud"></div><div class="rain"></div></div>');
-					break;
-					case "light rain" :
-					$('.iconDesc').append('<div class="icon rainy"><div class="cloud"></div><div class="rain"></div></div>');
-					break;
-
-				}
-
-			});
-
+	} else {
+		$('.temp .deg').text(tempInCelcius)
+		$('.temp .unit').text('C')
+		$('.temp').attr('class', 'temp celsius');
 	}
-	getLocation();
+}
 
+function getIcons (description){
+	switch (description) {
+		case "few clouds" :
+		$('.iconDesc').append('<div class="icon cloudy"><div class="cloud"></div><div class="cloud"></div></div>');
+		break;
+		case "thunderstorm" :
+		$('.iconDesc').append('<div class="icon thunder-storm"><div class="cloud"></div><div class="lightning"></div></div>');
+		break;
+		case "scattered clouds" :
+		$('.iconDesc').append('<div class="icon cloudy"><div class="cloud"></div><div class="sun"></div><div class="rain"></div></div>');
+		break;
+		case "sunny" :
+		$('.iconDesc').append('<div class="icon sunny"><div class="sun"></div></div>');
+		break;
+		case "rainy" :
+		$('.iconDesc').append('<div class="icon rainy"><div class="cloud"></div><div class="rain"></div></div>');
+		break;
+		case "light rain" :
+		$('.iconDesc').append('<div class="icon rainy"><div class="cloud"></div><div class="rain"></div></div>');
+		break;
+	}
+}
+
+function updateUI (data, position){
+	console.log(data)
+	$('.loader').hide();
+	$('.location').text(data.name + ', ' + data.sys.country);
+	$('.deg').text(data.main.temp)
+	$('.unit').text('C')
+	$('.temp').show()
+	$('.windSpeed').text(data.wind.speed + ' mph')
+	$('.humidity').text(data.main.humidity + '%')
+	$('.tempMinMax').text(data.main.temp_min + '°' + '/' + data.main.temp_max + '°')
+	$('.pressure').text(data.main.pressure + ' SI')
+	$('.visibility').text(data.visibility + ' mi')
+	$('.image').attr('src' , "https://maps.googleapis.com/maps/api/staticmap?center=" + position.lat + "," + position.lon + "&zoom=13&size=300x300&sensor=false") 
+	$('.weather').text(data.weather[0].description);
+	getIcons(data.weather[0].description);
+}
+
+$('.temp').click(function(){
+	convertTemperature()
 });
 
-
+getLocation();
---
+++ readme.md
@@ -0,0 +1,34 @@
+# Mini local weather App
+
+In this mini-project I have developed a small weather web application that shows the local weather in your position, as part of my full stack JavaScript training offered by [GOMYCODE Tunisia](https://www.gomycode.tn)
+
+## Project brief
+
+This is a small web application wich does the following:
+
+* Build a CodePen.io app that is funcionally similar to this [APP](http://codepen.io/FreeCodeCamp/full/bELRjV) .
+
+* Use whichever libraries or APIs we need. Give it my own personal style.
+
+* I can see the weather in my current location.
+
+* I can see a different icon or background image (e.g. snow mountain, hot desert) depending on the weather.
+
+* I can push a button to toggle between Fahrenheit and Celsius.
+
+* I can see a loading spinner while the application is loading the weather data.
+
+*add a small google map to your application. [Thid demo](https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/Using_geolocation) could be helpful
+<p align="center">
+	<img src="images/Capture.JPG">
+</p>
+
+## What I have learned
+
+* Using `@media screen and` to change css under a condition.
+
+* Use many API like geolocation api.
+
+* Manipulate the response of Ajax call using jQuery.
+
+* Devide the JS code into small functions that do one thing.
